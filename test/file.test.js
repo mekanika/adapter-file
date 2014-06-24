07e0bb989690eff3d3cdf1ffa5669418a207ad87
Clint Walker <clint@neonrelic.com>
2014-06-24 13:50:04 -0400

    Add core tests

--- test/file.test.js
+++ test/file.test.js
@@ -1,8 +1,46 @@
 
-var expect = require('chai').expect;
+var expect = require('chai').expect
+  , file = require('../index.js')
+  , fs = require('fs');
 
 describe('File Adapter', function () {
 
-  it('should have tests');
+  describe('core', function () {
+
+    it('exposes .exec() method', function () {
+      expect( file.exec ).to.be.a( 'function' );
+    });
+
+    it('requires a Qo with .action and .resource set', function (done) {
+
+      file.exec( null, function (err,res) {
+        expect( err ).to.exist;
+        expect( res ).to.not.exist;
+
+        file.exec( {action:'!'}, function (err,res) {
+          expect( err ).to.exist;
+          expect( res ).to.not.exist;
+
+          file.exec( {resource:'!'}, function (err,res) {
+            expect( err ).to.exist;
+            expect( res ).to.not.exist;
+            done();
+          });
+        });
+      });
+
+    });
+
+    it('returns cb(err) on unknown actions', function (done) {
+
+      file.exec( {action:'!', resource:'$'}, function (err, res) {
+        expect( err ).to.exist;
+        expect( res ).to.not.exist;
+        done();
+      })
+
+    });
+
+  });
 
 });
