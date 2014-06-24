5d0fab1468ce1bca5a138eaf4a1bf6c5b8db89a8
Clint Walker <clint@neonrelic.com>
2014-06-24 13:39:47 -0400

    Initial adapter core

---
+++ index.js
@@ -0,0 +1,32 @@
+
+/**
+ * Dependencies
+ */
+
+var adapter = require('mekanika-adapter');
+
+
+/**
+ * Export adapter
+ */
+
+module.exports = exports = adapter.new('file');
+
+
+/**
+ * Core adapter execution method
+ *
+ * @param req query object
+ * @param {Function} cb Callback passed `( err, results )`
+ */
+
+exports.exec = function ( req, cb ) {
+
+  // Check requireds
+  if (!req || !req.action || !req.resource) return cb('Invalid Query');
+
+  return exports[ req.action ]
+    ? exports[ req.action ]( req, cb )
+    : cb( 'No action defined:'+req.action );
+
+};
